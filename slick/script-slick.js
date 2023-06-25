e45f4519831ff8b2b1239241c8b6beb081cb85ee
Maks <Crossfade143@gmail.com>
2023-06-26 00:00:42 +0300

    fix adaptive

--- slick/script-slick.js
+++ slick/script-slick.js
@@ -2,3 +2,11 @@ $(".slider").slick({
   autoplay: true,
   autoplaySpeed: 3000,
 });
+
+$(window).resize(function () {
+  if ($(window).width() <= "600") {
+    console.log("Show");
+  } else {
+    console.log("Hide");
+  }
+});
