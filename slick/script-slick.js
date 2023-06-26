26fedfa2359616e5dd206ab310286dc417052bf3
Maks <Crossfade143@gmail.com>
2023-06-27 01:04:31 +0300

    dropdown-menu

--- slick/script-slick.js
+++ slick/script-slick.js
@@ -3,10 +3,20 @@ $(".slider").slick({
   autoplaySpeed: 3000,
 });
 
-$(window).resize(function () {
+//resize window
+$(window).on("load resize", function () {
   if ($(window).width() <= "600") {
     console.log("Show");
+    $("#button-menu").removeClass("hidden");
+    $("#button-menu").addClass("active");
   } else {
+    $("#button-menu").addClass("hidden");
+    $("#button-menu").removeClass("active");
     console.log("Hide");
   }
+});
+
+//Show menu
+$("#button-menu").on("click", () => {
+  $("#menu").toggleClass("hidden", "active");
 });
