8d8b4b0f4540ffe0f467d559d565f95f8cd0b1e4
Maks <Crossfade143@gmail.com>
2023-06-29 22:02:47 +0300

    refactoring code

---
+++ script/showDropdownMenu.js
@@ -0,0 +1,50 @@
+//resize window
+$(window).on("load resize", function () {
+  if ($(window).width() <= "768") {
+    console.log("Show");
+    $("#open-buttom").removeClass("hidden");
+    $("#open-buttom").addClass("active");
+    $("#header-buttons").removeClass("hidden");
+    $("#header-buttons").addClass("active");
+  } else {
+    $("#open-buttom").addClass("hidden");
+    $("#open-buttom").removeClass("active");
+    $("#header-buttons").addClass("hidden");
+    $("#header-buttons").removeClass("active");
+    console.log("Hide");
+  }
+});
+
+// hide dropdown menu when resize window
+$(window).resize(function () {
+  if ($(window).width() <= "768") {
+    console.log("Hide-menu");
+    $("#dropdown-menu").addClass("hidden");
+    $("#dropdown-menu").removeClass("active");
+
+    $("#close-buttom").addClass("hidden");
+    $("#close-buttom").removeClass("active");
+  }
+});
+
+//Show menu
+$("#open-buttom").on("click", () => {
+  $("#dropdown-menu").toggleClass("hidden", "active");
+
+  $("#open-buttom").addClass("hidden");
+  $("#open-buttom").removeClass("active");
+
+  $("#close-buttom").addClass("active");
+  $("#close-buttom").removeClass("hidden");
+});
+
+//close menu
+$("#close-buttom").on("click", () => {
+  $("#dropdown-menu").toggleClass("hidden", "active");
+
+  $("#close-buttom").addClass("hidden");
+  $("#close-buttom").removeClass("active");
+
+  $("#open-buttom").addClass("active");
+  $("#open-buttom").removeClass("hidden");
+});
--- slick/script-slick.js
+++ slick/script-slick.js
@@ -2,21 +2,3 @@ $(".slider").slick({
   autoplay: true,
   autoplaySpeed: 3000,
 });
-
-//resize window
-$(window).on("load resize", function () {
-  if ($(window).width() <= "600") {
-    console.log("Show");
-    $("#button-menu").removeClass("hidden");
-    $("#button-menu").addClass("active");
-  } else {
-    $("#button-menu").addClass("hidden");
-    $("#button-menu").removeClass("active");
-    console.log("Hide");
-  }
-});
-
-//Show menu
-$("#button-menu").on("click", () => {
-  $("#menu").toggleClass("hidden", "active");
-});
